1b380e9ecd64b873d2b45cafcb49ab4107dbdde4
Hemant Sachdeva <hemant.evolver@gmail.com>
2021-11-29 21:43:06 +0530

    Add second dice and roll both with single button
    * problem statement https://developer.android.com/codelabs/basic-android-kotlin-training-dice-roller-images#9
    
    Signed-off-by: Hemant Sachdeva <hemant.evolver@gmail.com>

--- app/src/main/java/com/hemantsachdeva/diceroller/MainActivity.kt
+++ app/src/main/java/com/hemantsachdeva/diceroller/MainActivity.kt
@@ -25,7 +25,7 @@ class MainActivity : AppCompatActivity() {
         rollDice()
 
         // Set a click listener on the button to roll the dice when the user taps the button
-        rollButton.setOnClickListener{
+        rollButton.setOnClickListener {
             rollDice()
         }
     }
@@ -36,22 +36,32 @@ class MainActivity : AppCompatActivity() {
     private fun rollDice() {
         // Create new Dice object with 6 sides and roll it
         val dice = Dice(6)
-        val diceRoll = dice.roll()
 
         // Find the ImageView in the layout
-        val diceImage: ImageView = findViewById(R.id.imageView)
+        val leftDiceImage: ImageView = findViewById(R.id.leftDice)
+        val rightDiceImage: ImageView = findViewById(R.id.rightDice)
 
         // Determine which drawable resource ID to use based on the dice roll
-        val drawableResource = when (diceRoll) {
-            1 -> R.drawable.dice_1
-            2 -> R.drawable.dice_2
-            3 -> R.drawable.dice_3
-            4 -> R.drawable.dice_4
-            5 -> R.drawable.dice_5
-            else -> R.drawable.dice_6
+        var pass: Int = 1
+
+        // Update dices one by one to get different number on each
+        repeat(2) {
+            val drawableResource = when (dice.roll()) {
+                1 -> R.drawable.dice_1
+                2 -> R.drawable.dice_2
+                3 -> R.drawable.dice_3
+                4 -> R.drawable.dice_4
+                5 -> R.drawable.dice_5
+                else -> R.drawable.dice_6
+            }
+            // Update the ImageView with the correct drawable resource ID
+            if (pass == 1) {
+                leftDiceImage.setImageResource(drawableResource)
+            } else {
+                rightDiceImage.setImageResource(drawableResource)
+            }
+            pass += 1
         }
-        // Update the ImageView with the correct drawable resource ID
-        diceImage.setImageResource(drawableResource)
     }
 }
 
